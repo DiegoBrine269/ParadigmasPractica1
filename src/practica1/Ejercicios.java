fcd18807ee67a9ba80815d8b7f46ba96793e69b6
Ricardo Mora <rurimoca@gmail.com>
2021-03-18 17:04:39 -0600

    Se agrega el ejercicio 1

--- src/practica1/Ejercicios.java
+++ src/practica1/Ejercicios.java
@@ -1,10 +1,19 @@
 package practica1;
 import java.lang.Math;
+import java.util.List; //Biblioteca para primer y segundo ejercicio
 
 public class Ejercicios {
     
     public void ejercicio1 (){
-
+        List<Integer> numeros = List.of(18,6,4,15,55,78,12,9,8);
+        int contador = 0;
+        
+        for(int numero : numeros){
+            if(numero > 10){
+                contador++;
+            }
+        }
+        System.out.println(contador);
     }
 
     public void ejercicio2 (){
